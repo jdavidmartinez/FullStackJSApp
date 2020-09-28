519d7c5801c529af61a5b1fdeed594f7c355755b
Juan David Martinez <jdavid.martinez@gmail.com>
2020-09-27 23:41:45 -0500

    Javascript FullStack #13, HTTP Delete & eliminacion de Imagenes

---
+++ frontend/services/BookService.js
@@ -0,0 +1,37 @@
+
+
+class BookService {
+
+    constructor() {
+        this.URI= 'http://localhost:3000/api/books'
+        
+    }
+
+    async getBooks(){
+        const response= await fetch(this.URI);
+        const books = await response.json();
+        return books;
+       
+    }
+
+   async postBook(book){
+       const response =  await fetch(this.URI,{
+            method: 'POST',
+            body: book
+        });
+        const data = await response.json();
+        console.log('Book Saved')
+        
+    }
+
+    async deleteBook(bookId){
+        const response= await fetch(`${this.URI}/${bookId}`,{
+            headers:{
+                "Content-Type": "application/json"
+            },
+            method:"DELETE"
+        });
+       
+    }
+}
+module.exports = BookService;
